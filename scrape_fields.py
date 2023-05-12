332d6e3a5171834fe6a8d9858a5996cc08df8df5
Sandev <sandevdewthilina2000@gmail.com>
2023-05-12 18:01:59 +0530

    edge detection

--- scrape_fields.py
+++ scrape_fields.py
@@ -5,8 +5,55 @@ from pdf2image import convert_from_path
 from PIL import Image
 from pytesseract import pytesseract
 from google.cloud import vision
+import cv2
+import numpy as np
 
 tesseract_path = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+
+
+def extract_boundary_and_save(img, image_path):
+    # if i != 0:
+    #   continue
+    img = cv2.imread(image_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    blur = cv2.GaussianBlur(gray, (7, 7), cv2.BORDER_WRAP)
+    thresh = cv2.adaptiveThreshold(blur, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2)
+    contours, hierarchy = cv2.findContours(thresh, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+    max_area = 0
+    box = []
+    # Get the height and width of the image
+    parent_height, parent_width, channels = img.shape
+
+    my_contoure = []
+
+    for cnt in contours:
+        approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
+        if len(approx) == 4:
+            rect = cv2.minAreaRect(approx)
+            area = cv2.contourArea(approx)
+
+            height = int(rect[1][1])
+            width = int(rect[1][0])
+
+            real_width = width
+            real_height = height
+
+            if height > width:
+                real_width = height
+                real_height = width
+
+            if area > max_area and parent_width - 5 > real_width and parent_height - 5 > real_height:
+                max_area = area
+                box = cv2.boxPoints(rect)
+                box = np.int0(box)
+                my_contoure = approx
+    # cv2.drawContours(img, [box], -1, (0,0,255), 3)
+    x, y, w, h = cv2.boundingRect(my_contoure)
+    cropped_img = img[y:y + h, x:x + w]
+    new_width = 3000
+    new_height = int(h * (new_width / w))
+    resized = cv2.resize(cropped_img, (new_width, new_height))
+    cv2.imwrite(image_path, resized)
 
 
 def get_text_of_area(body):
@@ -47,7 +94,8 @@ def get_text_of_area(body):
     for i in range(len(images)):
         file = 'page' + str(i) + '.jpg'
         image_path = os.path.join(pages_directory_path, file)
-        images[i].save(image_path, 'JPEG')
+        images[i].save(image_path)
+        extract_boundary_and_save(images[i], image_path)
 
     # initialize pytesseract
     pytesseract.tesseract_cmd = tesseract_path
